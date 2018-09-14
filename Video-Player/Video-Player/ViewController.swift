3ccf715908c98ae7aa68f125122d46d116367778
TaiHsin <peterlee0466@gmail.com>
2018-09-14 12:09:02 +0800

    Applied url search and auto play video function

--- Video-Player/Video-Player/ViewController.swift
+++ Video-Player/Video-Player/ViewController.swift
@@ -12,7 +12,7 @@ import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet weak var searchButton: UIButton!
-    @IBOutlet weak var inputUrlTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var totalTimeLabel: UILabel!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
@@ -22,9 +22,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var fullScreenButton: UIButton!
     @IBOutlet weak var videoView: UIView!
     
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+//        guard let url = URL(string: "https://s3-ap-northeast-1.amazonaws.com/mid-exam/Video/taeyeon.mp4") else { return }
+//        player = AVPlayer(url: url)
+//
+//        playerLayer = AVPlayerLayer(player: player)
+//
+//        // Resize when rotate
+//        playerLayer.videoGravity = .resize
+//
+//        videoView.layer.addSublayer(playerLayer)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,12 +49,39 @@ class ViewController: UIViewController {
         searchButton.layer.borderWidth = 1
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//
+//        player.play()
+//    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Define videoView is the playerLayer's frame
+        
+    }
+    
+    @IBAction func searchAndPlay(_ sender: Any) {
+        
+        guard let url = urlTextField.text else { return }
+        print(url)
+        guard let videoUrl = URL(string: url) else { return }
+        player = AVPlayer(url: videoUrl)
+        
+        playerLayer = AVPlayerLayer(player: player)
+        
+        // Resize when rotate
+        playerLayer.videoGravity = .resize
+        
+        videoView.layer.addSublayer(playerLayer)
+        playerLayer.frame = videoView.bounds
+        player.play()
 
     }
     
     @IBAction func playVideo(_ sender: Any) {
+        player.pause()
     }
     
     @IBAction func playForward(_ sender: Any) {
