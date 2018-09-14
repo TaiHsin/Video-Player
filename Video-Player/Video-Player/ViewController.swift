608ed52a17c13bc678178e353e2d38e10faccd19
TaiHsin <peterlee0466@gmail.com>
2018-09-14 15:01:49 +0800

    Applied slidBar and show duration/ current time function

--- Video-Player/Video-Player/ViewController.swift
+++ Video-Player/Video-Player/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var urlTextField: UITextField!
-    @IBOutlet weak var totalTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var volumeButton: UIButton!
     @IBOutlet weak var fullScreenButton: UIButton!
     @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var timeSlider: UISlider!
     
     var player = AVPlayer()
     var playerLayer =  AVPlayerLayer()
@@ -30,6 +31,7 @@ class ViewController: UIViewController {
         
         // Resize when rotate
         playerLayer.videoGravity = .resize
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,10 +51,23 @@ class ViewController: UIViewController {
         
         // Define videoView is the playerLayer's frame
         playerLayer.frame = videoView.bounds
-        
+    }
+    
+    func addTimeObserver() {
+        let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        let mainQueue = DispatchQueue.main
+        _ = player.addPeriodicTimeObserver(forInterval: interval, queue: mainQueue, using: { [weak self] time in
+            guard let currentItem = self?.player.currentItem else { return }
+            self?.timeSlider.maximumValue = Float(currentItem.duration.seconds)
+            self?.timeSlider.minimumValue = 0
+            self?.timeSlider.value = Float(currentItem.currentTime().seconds)
+            self?.currentTimeLabel.text = self?.getTimeString(from: currentItem.currentTime())
+        })
     }
     
     @IBAction func searchAndPlay(_ sender: Any) {
+        
+
         guard let url = urlTextField.text else { return }
         print(url)
         guard let videoUrl = URL(string: url) else { return }
@@ -60,10 +75,12 @@ class ViewController: UIViewController {
         player = AVPlayer(url: videoUrl)
         playerLayer = AVPlayerLayer(player: player)
         videoView.layer.addSublayer(playerLayer)
+        addTimeObserver()
+        player.currentItem?.addObserver(self, forKeyPath: "duration", options: [.new, .initial], context: nil)
+
+        playButton.setImage(#imageLiteral(resourceName: "btn_stop"), for: .normal)
         
         player.play()
-        
-        playButton.setImage(#imageLiteral(resourceName: "btn_stop"), for: .normal)
         
         urlTextField.text = ""
 
@@ -78,26 +95,17 @@ class ViewController: UIViewController {
             player.pause()
             sender.setImage(#imageLiteral(resourceName: "btn_play"), for: .normal)
         }
-        
-//        if isVideoPlaying {
-//            player.pause()
-//            sender.setImage(#imageLiteral(resourceName: "btn_play"), for: .normal)
-//        } else {
-//            player.play()
-//            sender.setImage(#imageLiteral(resourceName: "btn_stop"), for: .normal)
-//        }
-//        isVideoPlaying = !isVideoPlaying
     }
     
     @IBAction func playForward(_ sender: Any) {
         
         // Get total duration of video
-        guard let duration = player.currentItem?.duration else { return }
+        guard let duration = player.currentItem?.duration else { return } // currentItem?
         let currentTime = CMTimeGetSeconds(player.currentTime())
         let newTime = currentTime + 10.0
         
         if newTime < (CMTimeGetSeconds(duration) - 10.0) {
-            let time: CMTime = CMTimeMake(Int64(newTime * 1000), 1000)
+            let time: CMTime = CMTimeMake(Int64(newTime * 1000), 1000) // why devide by 1000?
             player.seek(to: time)
         }
     }
@@ -114,6 +122,28 @@ class ViewController: UIViewController {
     
 //    @IBAction func setFullScreen(_ sender: Any) {
 //    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        player.seek(to: CMTimeMake(Int64(sender.value * 1000), 1000))
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "duration", let duration = player.currentItem?.duration.seconds, duration > 0.0 {
+            self.durationLabel.text = getTimeString(from: player.currentItem!.duration)
+        }
+    }
+    
+    func getTimeString(from time: CMTime) -> String {
+        let totalSeconds = CMTimeGetSeconds(time)
+        let hours = Int(totalSeconds / 3600)
+        let minutes = Int(totalSeconds / 60) % 60
+        let seconds = Int(totalSeconds.truncatingRemainder(dividingBy: 60)) //??
+        
+        if hours > 0 {
+            return String(format: "%i:%02i:%02i", arguments: [hours, minutes, seconds])
+        } else {
+            return String(format: "%02i:%02i", arguments: [minutes, seconds])
+        }
+    }
     
     @IBAction func setVolume(_ sender: Any) {
     }
